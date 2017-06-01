713fbaedf42869c6b9b8e9cacb0a274b4a93aee6
ibrahimkayaa <ibrahimkaya@outlook.com>
2017-06-02 01:55:24 +0300

    Character Animation Jump
    -Character Jump animation Setup

--- endlessRunnerSCC/Assets/Scripts/CharacterMovement.cs
+++ endlessRunnerSCC/Assets/Scripts/CharacterMovement.cs
@@ -8,14 +8,21 @@ public class CharacterMovement : MonoBehaviour {
 
 	//Public Variables
 	public float speed;
+	public Vector2 jumpForce;
 	public bool isMoving;
+	public bool isGrounded;
+	public bool inAir;
+	public Transform groundCheckPoint;
+	public float groundCheckRadius;
+	public LayerMask groundCheckLayerMask;
+
 
 	//Private Variables
 	Animator animController;
 	Rigidbody charBody;
 	CapsuleCollider charCapsul;
 	Vector3 charVelocity;
-
+	Collider[] groundCollidersArray;
 
 
 	void Awake () {
@@ -28,12 +35,32 @@ public class CharacterMovement : MonoBehaviour {
 	
 
 	void Update () {
+		charVelocity = Vector3.zero;
+
+		groundCollidersArray = Physics.OverlapSphere (groundCheckPoint.position, groundCheckRadius, groundCheckLayerMask);
+
+		isGrounded = (groundCollidersArray.Length > 0) ? true : false;
+
 
 		if(Input.GetKeyDown (KeyCode.Space) && !isMoving){
 			isMoving = true;
 			animController.SetFloat ("Speed",speed);
 		}
 
+		if(Input.GetKeyDown (KeyCode.J) && isGrounded){
+
+
+
+			charBody.AddForce (0f, jumpForce.y * Time.fixedDeltaTime, jumpForce.x * Time.fixedDeltaTime, ForceMode.Impulse);
+			animController.SetTrigger ("Jump");
+			inAir = true;
+
+		}
+
+		if(isMoving && isGrounded ){
+			inAir = false;
+			animController.SetTrigger ("Jump_Roll");
+		}
 
 
 	}
@@ -47,7 +74,7 @@ public class CharacterMovement : MonoBehaviour {
 
 	void RunLoop(){
 
-		charVelocity = Vector3.zero;
+
 		charVelocity.z = speed;
 		/*charBody.velocity = charVelocity * Time.fixedDeltaTime;*/
 		transform.position += charVelocity * Time.fixedDeltaTime;
